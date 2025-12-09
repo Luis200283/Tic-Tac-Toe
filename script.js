f7baac47d863b9376d02542db10e0ad6d808923f
Luis Hernandez <luishernandezopsu2019@gmail.com>
2025-12-09 17:09:00 -0400

    version 2.0 del juego de tres en ralla (se emjoro la estructura del procesameinto de los datos incuyendo  closers, IIFE y metodos asociados a objetos

--- script.js
+++ script.js
@@ -1,55 +1,53 @@
-function getArrays(container) {
-    let matrixOrder = 3;
-    const rowArray = getBoardArray()
-    const columnArray = getColumnArray(rowArray);
-    const diagonalsArray = getDiagonalsArray(rowArray)
-
-    function getBoardArray() {
-        const array = []
-        let index = 0
-        for (let i = 0; i < matrixOrder; i++) {
-            array.push([])
-            for (let j = 0; j < matrixOrder; j++) {
-                array[i].push(container[index].textContent)
-                index++
+const gameBoard = function (gridsBoard) {
+    const array = []
+    const matrixOrder = 3
+    return {
+        rows() {
+            if (array.length === 0) {
+                let index = 0
+                for (let i = 0; i < matrixOrder; i++) {
+                    array.push([])
+                    for (let j = 0; j < matrixOrder; j++) {
+                        array[i].push(gridsBoard[index].textContent)
+                        index++
+                    }
+                }
+                return array
             }
-        }
-        return array
-    }
-
-    function getColumnArray(array) {
-        const newArray = [];
-        for (let i = 0; i < matrixOrder; i++) {
-            newArray.push([])
-        }
-        for (let i = 0; i < array.length; i++) {
-            for (let j = 0; j < array.length; j++) {
-                newArray[j].push(array[i][j])
+            return array
+        },
+        columns() {
+            if (array.length === 0) this.rows()
+            const newArray = [];
+            for (let i = 0; i < matrixOrder; i++) {
+                newArray.push([])
+                for (let j = 0; j < matrixOrder; j++) {
+                    newArray[i].push(array[j][i])
+                }
             }
+            return newArray
+        },
+        diagonals() {
+            if (array.length === 0) this.rows()
+            const newArray = [[], [],]
+            for (let i = 0; i < array.length; i++) {
+                newArray[0].push(array[i][i])
+                newArray[1].push(array[i][array.length - 1 - i])
+            }
+            return newArray
         }
-        return newArray
     }
 
-    function getDiagonalsArray(array) {
-        const newArray = [[], [],]
-        for (let i = 0; i < array.length; i++) {
-            newArray[0].push(array[i][i])
-            newArray[1].push(array[i][array.length - 1 - i])
-        }
-        return newArray
-    }
-
-    searchMatch(rowArray, columnArray, diagonalsArray)
 }
 
-function searchMatch(rows, columns, diagonals) {
+
+function searchMatch(gridsBoard) {
+    let playerWin = false
 
     function rowMatch(array) {
         let allEqual = true
-
         array.forEach((element, index) => {
             const firstElement = array[0]
-
             if (index > 0 && element !== firstElement) {
                 allEqual = false
             }
@@ -57,49 +55,77 @@ function searchMatch(rows, columns, diagonals) {
                 allEqual = false
             }
         })
-
         if (allEqual) {
             alert(`${array[0]} ha ganado`)
+            clearScreen()
+            playerWin = true
+        }
+    }
+
+    function searchTie(array) {
+        let counter = 0
+        array.forEach((element) => {
+            element.forEach((val) => {
+                if (val !== '') ++counter
+            })
+        })
+        if (counter === 9 && !playerWin) {
+            alert('They are tied the game')
             clearScreen()
         }
     }
 
-    columns.forEach((row) => rowMatch(row))
-    rows.forEach((row) => rowMatch(row))
-    diagonals.forEach((row) => rowMatch(row))
+    function clearScreen() {
+        const gridsBoard = document.getElementsByClassName('grid-board')
+        const button = document.querySelector('button')
 
+        function clear() {
+            for (let i = 0; i < gridsBoard.length; i++) {
+                console.log(gridsBoard[i])
+                gridsBoard[i].textContent = ''
+            }
+        }
+
+        setTimeout(clear, 500)
+        button.addEventListener('click', clear)
+    }
+
+    gameBoard(gridsBoard).rows().forEach((row) => { rowMatch(row) })
+    gameBoard(gridsBoard).columns().forEach((row) => { rowMatch(row) })
+    gameBoard(gridsBoard).diagonals().forEach((row) => { rowMatch(row) })
+    searchTie(gameBoard(gridsBoard).rows())
 }
+
+
 
 function screeView() {
     const gameBoard = document.getElementById('game-board')
-    const gridsBoard = document.getElementsByClassName('grid-board')
-
-    let turn = 1
+    let gridsBoard = document.getElementsByClassName('grid-board')
+    let turnCounter = 1
     let market = [0]
 
     gameBoard.addEventListener('click', (event) => {
         if (event.target.title !== 'container') {
             const element = event.target
-            const elementTitle = Number(element.title)
-            let isMarket = true
-
+            const gridNumber = Number(element.title)
+            let gridIsMarket = true
 
             market.forEach((val) => {
-                isMarket = val !== elementTitle ? false : true
+                gridIsMarket = val !== gridNumber ? false : true
             })
 
-            if (!isMarket) {
-                market.push(Number(elementTitle))
-                if (turn) {
+            if (!gridIsMarket) {
+                market.push(Number(gridNumber))
+                if (turnCounter) {
                     element.textContent = "X"
-                    turn--
+                    turnCounter--
                 }
                 else {
                     element.textContent = "O"
-                    turn++
+                    turnCounter++
                 }
             }
-            getArrays(gridsBoard)
+            searchMatch(gridsBoard)
         }
     })
 
@@ -107,16 +133,3 @@ function screeView() {
 
 }
 screeView()
-
-function clearScreen() {
-    const gridsBoard = document.getElementsByClassName('grid-board')
-    const button = document.querySelector('button')
-
-    button.addEventListener('click', () => {
-        for (let i = 0; i < gridsBoard.length; i++) {
-            console.log(gridsBoard[i])
-            gridsBoard[i].textContent = ''
-        }
-    })
-}
-
